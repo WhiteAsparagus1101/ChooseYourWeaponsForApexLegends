03b77874b7c4d1634caa84f7aaac2ea88bcf0506
WhiteAsparagus1101 <55292680+WhiteAsparagus1101@users.noreply.github.com>
2021-12-24 04:14:12 +0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,38 +1,237 @@
-let weapons = [
-    "HAVOC Rifle",
-    "VK-47 Flatline",
-    "Hemlok Burst AR",
-    "R-301 Carbine",
-    "Alternator SMG",
-    "Prowler Burst PDW",
-    "R-99 SMG",
-    "Volt SMG",
-    "C.A.R. SMG",
-    "Devotion LMG",
-    "L-STAR EMG",
-    "M600 Spitfire",
-    "Rampage LMG",
-    "G7 Scout",
-    "Triple Take",
-    "30-30 Repeater",
-    "Bocek Compound Bow",
-    "Charge Rifle",
-    "Longbow DMR",
-    "Kraber .50-Cal Sniper",
-    "Sentinel",
-    "EVA-8 Auto",
-    "Mastiff Shotgun",
-    "Mozambique Shotgun",
-    "Peacekeeper",
-    "RE-45 Auto",
-    "P2020",
-    "Wingman"
+let weapons = [{
+        "name": "HAVOC Rifle",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "hop up": ["none", "Turbocharger"],
+        "supply drop": false
+    },
+    {
+        "name": "VK-47 Flatline",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false,
+    },
+    {
+        "name": "Hemlok Burst AR",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "hop up": ["none", "Boosted Loader"],
+        "supply drop": false
+    },
+    {
+        "name": "R-301 Carbine",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "Alternator SMG",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "supply drop": true,
+    },
+    {
+        "name": "Prowler Burst PDW",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "R-99 SMG",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "Volt SMG",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "C.A.R. SMG",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "Devotion LMG",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "hop up": ["none", "Turbocharger"],
+        "supply drop": false
+    },
+    {
+        "name": "L-STAR EMG",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "M600 Spitfire",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "supply drop": true
+    },
+    {
+        "name": "Rampage LMG",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "standard stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "G7 Scout",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "supply drop": true
+    },
+    {
+        "name": "Triple Take",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "sniper stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "30-30 Repeater",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\"", "2x-4x Variable AOG"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "sniper stock": ["none", "Level1", "Level2", "Level3"],
+        "hop up": ["none", "Shatter Caps"],
+        "supply drop": false
+    },
+    {
+        "name": "Bocek Compound Bow",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "3x HCOG \"Ranger\""],
+        "hop up": ["none", "Deadeye's Tempo", "Shatter Caps"],
+        "supply drop": false
+    },
+    {
+        "name": "Charge Rifle",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "6x Sniper", "3x HCOG \"Ranger\"", "2x-4x Variable AOG", "4x-8x Variable Sniper", "4x-10x Digital Sniper Threat"],
+        "sniper stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "Longbow DMR",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "6x Sniper", "3x HCOG \"Ranger\"", "2x-4x Variable AOG", "4x-8x Variable Sniper", "4x-10x Digital Sniper Threat"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "sniper stock": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "Kraber .50-Cal Sniper",
+        "supply drop": true
+    },
+    {
+        "name": "Sentinel",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "6x Sniper", "3x HCOG \"Ranger\"", "2x-4x Variable AOG", "4x-8x Variable Sniper", "4x-10x Digital Sniper Threat"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "sniper stock": ["none", "Level1", "Level2", "Level3"],
+        "hop up": ["none", "Deadeye's Tempo"],
+        "supply drop": false
+    },
+    {
+        "name": "EVA-8 Auto",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "shotgun bolt": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "Mastiff Shotgun",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "shotgun bolt": ["none", "Level1", "Level2", "Level3"],
+        "hop up": ["none", "Dual Shell"],
+        "supply drop": false
+    },
+    {
+        "name": "Mozambique Shotgun",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "shotgun bolt": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "Peacekeeper",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "shotgun bolt": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "RE-45 Auto",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "barrel stabilizer": ["none", "Level1", "Level2", "Level3"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "P2020",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "supply drop": false
+    },
+    {
+        "name": "Wingman",
+        "optic": ["none", "1x HCOG \"Classic\"", "1x Holo", "2x HCOG \"Bruiser\"", "1x-2x Variable Holo", "1x Digital Threat"],
+        "extended mag": ["none", "Level1", "Level2", "Level3"],
+        "hop up": ["none", "Boosted Loader"],
+        "supply drop": false
+    }
 ];
 
-for (let i = 0; i < 30; i++) {
+let includeSupplyDrop = true;
 
-    let n1 = Math.floor(Math.random() * weapons.length);
-    let n2 = Math.floor(Math.random() * weapons.length);
+let results = [];
 
-    console.log(weapons[n1] + ", " + weapons[n2]);
+for (let i = 0; i < 2; i++) {
+    let result = [];
+    let weapon = weapons[Math.floor(Math.random() * weapons.length)];
+    if (!includeSupplyDrop) {
+        while (weapon["supply drop"] == true) {
+            weapon = weapons[Math.floor(Math.random() * weapons.length)];
+        }
+    }
+    result.push(["Name", weapon["name"]]);
+    if (weapon["optic"] != null) {
+        result.push(["Optic", weapon["optic"][Math.floor(Math.random() * weapon["optic"].length)]]);
+    }
+    if (weapon["barrel stabilizer"] != null) {
+        result.push(["Barrel Stabilizer", weapon["barrel stabilizer"][Math.floor(Math.random() * weapon["barrel stabilizer"].length)]]);
+    }
+    if (weapon["extended mag"] != null) {
+        result.push(["Extended Mag", weapon["extended mag"][Math.floor(Math.random() * weapon["extended mag"].length)]]);
+    }
+    if (weapon["standard stock"] != null) {
+        result.push(["Standard Stock", weapon["standard stock"][Math.floor(Math.random() * weapon["standard stock"].length)]]);
+    }
+    if (weapon["sniper stock"] != null) {
+        result.push(["Sniper stock", weapon["sniper stock"][Math.floor(Math.random() * weapon["sniper stock"].length)]]);
+    }
+    if (weapon["hop up"] != null) {
+        result.push(["Hop-Up", weapon["hop up"][Math.floor(Math.random() * weapon["hop up"].length)]]);
+    }
+    if (weapon["shotgun bolt"] != null) {
+        result.push(["Shotgun Bolt", weapon["shotgun bolt"][Math.floor(Math.random() * weapon["shotgun bolt"].length)]]);
+    }
+
+
+    results.push(result);
 }
+
+console.log(results);
